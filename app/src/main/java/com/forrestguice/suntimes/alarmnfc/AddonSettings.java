188c6ce9ec762792908eec2482c7076c22369706
Forrest Guice <forrestguice@users.noreply.github.com>
2022-12-17 00:50:15 -0700

    attr
    icActionSettings

--- app/src/main/java/com/forrestguice/suntimes/alarmnfc/AddonSettings.java
+++ app/src/main/java/com/forrestguice/suntimes/alarmnfc/AddonSettings.java
@@ -22,9 +22,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatDelegate;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import android.util.Base64;
 
 import com.forrestguice.suntimes.addon.SuntimesInfo;
